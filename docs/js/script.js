658c869c06b778ccbabab4232d0b9382c24f15d9
Lenin Zapata <lenin.zapata.1993@gmail.com>
2025-08-13 23:09:17 -0500

    Create script.js

---
+++ docs/js/script.js
@@ -0,0 +1,29 @@
+function loadContent(page) {
+    fetch(page)
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('content').innerHTML = html;
+    })
+    .catch(error => {
+        document.getElementById('content').innerHTML = "<p>Error al cargar el contenido.</p>";
+    });
+}
+
+function alt_image() {
+  let estado = true;
+
+  setInterval(() => {
+    const img = document.getElementById("imagen");
+
+    img.style.opacity = 0; // fade out
+    if (estado) {
+      img.style.backgroundImage = 'url("../images/profile_picture.jpg")';
+      img.alt = "Profile Picture";
+    } else {
+      img.style.backgroundImage = 'url("../images/logo.jpg")';
+      img.alt = "Logo";
+    }
+    estado = !estado;
+    img.style.opacity = 1; // fade in
+  }, 5000);
+}
