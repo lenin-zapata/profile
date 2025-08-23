e9bbb2d53cceb4f67cc6b5f0dea4dd7121baad1e
Lenin Zapata <lenin.zapata.1993@gmail.com>
2025-08-23 11:56:06 -0500

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,13 +2,13 @@ function alt_image() {
   let estado = true;
 
   setInterval(() => {
-    const img = document.getElementById("imagen");
+    const img = document.getElementById("profile-image");
 
     if (estado) {
-      img.style.backgroundImage = 'url("./images/profile_picture.jpg")';
+      img.style.backgroundImage = 'url("../images/profile_picture.jpg")';
       img.alt = "Profile Picture";
     } else {
-      img.style.backgroundImage = 'url("./images/logo.jpg")';
+      img.style.backgroundImage = 'url("../images/logo.jpg")';
       img.alt = "Logo";
     }
     estado = !estado;
@@ -18,4 +18,6 @@ function alt_image() {
 document.addEventListener('DOMContentLoaded', function() {
   // Actualizar el año en el footer
   document.getElementById('year').innerText = new Date().getFullYear();
+
+  alt_image();
 })
